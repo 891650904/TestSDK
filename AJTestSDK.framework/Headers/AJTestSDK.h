100e5996c7144d6d8dd8081318a94230a188b942
891650904 <891650904@qq.com>
2018-11-28 18:56:03 +0800

    提交

---
+++ AJTestSDK.framework/Headers/AJTestSDK.h
@@ -0,0 +1,27 @@
+//
+//  AJTestSDK.h
+//  AJTestSDK
+//
+//  Created by lishuijiao on 2018/11/14.
+//  Copyright © 2018年 Lisj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AJTestSDK.
+FOUNDATION_EXPORT double AJTestSDKVersionNumber;
+
+//! Project version string for AJTestSDK.
+FOUNDATION_EXPORT const unsigned char AJTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AJTestSDK/PublicHeader.h>
+
+#import <Foundation/Foundation.h>
+
+#import "QNConfiguration.h"
+#import "QNFileRecorder.h"
+#import "QNResponseInfo.h"
+#import "QNUploadManager.h"
+#import "QNUploadOption.h"
+#import "QNUrlSafeBase64.h"
+
